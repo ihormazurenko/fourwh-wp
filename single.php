349b39742ea083a7466b47bfb0eec2ce90657610
Ihor <mazurenko.ihor@gmail.com>
2018-12-11 22:25:09 +0200

    Single Page

--- single.php
+++ single.php
@@ -1,28 +1,16 @@
 <?php get_header(); ?>
 
-    <div class="content-wrap">
-        <div class="content">
+    <section class="section section-content content-wrapper">
+        <div class="container">
+            <?php the_title('<h1 class="section-title">', '</h1>'); ?>
+            <div class="content">
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                    <?php the_content(); ?>
 
-                <article>
-
-                    <header>
-                        <?php the_title('<h1>', '</h1>'); ?>
-                    </header>
-
-                    <section>
-                        <?php the_content(); ?>
-                    </section>
-
-                </article>
-
-            <?php endwhile; else: endif; ?>
-
-        </div><!--/content-->
-
-        <?php get_sidebar(); ?>
-
-    </div><!--/content-wrap-->
+                <?php endwhile; else: endif; ?>
+            </div>
+        </div>
+    </section>
 
 <?php get_footer(); ?>

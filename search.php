2c7aad7df1b594cc424dd51c4781d08cc3d28531
Ihor <mazurenko.ihor@gmail.com>
2018-12-13 15:21:39 +0200

    Search

--- search.php
+++ search.php
@@ -3,7 +3,7 @@
     <section class="section content-wrapper section-articles searsh">
         <div class="container">
 
-            <h1 class="section-title"><?php printf( esc_html__( 'Search Results for: %s', 'fw_campers' ), '<span>' . trim(get_search_query()) . '</span>' ); ?></h1>
+            <h1 class="section-title"><?php printf( esc_html__( 'Search Results for: %s', 'fw_campers' ), '<span><i>' . trim(get_search_query()) . '</i></span>' ); ?></h1>
 
             <div class="content-with-sidebar">
                 <div class="content-box">
@@ -20,7 +20,17 @@
                             get_template_part('inc/pagination');
 
                         } else {
-                            echo '<p class="no-results">Sorry, articles not found...</p>';
+                            if ( is_search() ) {
+                                ?>
+                                    <p class="search-sorry-text"><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'fw_campers' ); ?></p>
+                                    <?php get_search_form(); ?>
+                                <?php
+                            } else {
+                                ?>
+                                    <p class="search-sorry-text"><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'fw_campers' ); ?></p>
+                                    <?php get_search_form(); ?>
+                                <?php
+                            }
                         };
                     ?>
                 </div>

04a531163c6523016441710dac399a82cdabe318
Ihor <mazurenko.ihor@gmail.com>
2018-12-22 13:47:09 +0200

    All model

---
+++ inc/loop-model.php
@@ -0,0 +1,39 @@
+<?php
+    $title = get_the_title();
+    $url = get_permalink();
+    $description = get_field('description');
+?>
+<li>
+    <div class="product-wrap">
+        <a href="<?php echo esc_url($url); ?>" title="<?php echo esc_attr($title); ?>">
+            <h3 class="product-title"><?php echo $title; ?></h3>
+        </a>
+        <div class="product-box">
+            <a href="<?php echo esc_url($url); ?>" title="<?php echo esc_attr($title); ?>">
+                <?php if (has_post_thumbnail()) { ?>
+                    <div class="product-img-wrap centered-img">
+                        <?php the_post_thumbnail('medium_large', array(
+                            'alt'   => esc_attr($title)
+                        )); ?>
+                    </div>
+                <?php } ?>
+            </a>
+            <div class="see-improvement-box">
+                <span class="see-improvement"><?php _e('See Improvement','fw_campers'); ?></span>
+            </div>
+            <?php if ( $description && is_array( $description ) && count( $description ) > 0 ) : ?>
+                <?php if ($description['short_description']) : ?>
+                    <div class="product-info-box content small">
+                        <?php echo $description['short_description']; ?>
+                    </div>
+                <?php endif; ?>
+                <?php if ($description['expanded_description']) : ?>
+                    <a href="#" class="more-info-btn" title="<?php esc_attr_e('More Info ','fw_campers'); ?>"><?php _e('More Info ','fw_campers'); ?><span class="arrow-down"></span></a>
+                    <div class="product-info-box more content small">
+                        <?php echo $description['expanded_description']; ?>
+                    </div>
+                <?php endif; ?>
+            <?php endif; ?>
+        </div>
+    </div>
+</li>
---
+++ page-models.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Template Name: Campers List
+ */
+get_header(); ?>
+
+    <?php get_template_part('inc/hero', 'banner'); ?>
+
+    <section class="section content-wrapper section-products">
+        <div class="container">
+
+            <?php get_template_part('inc/section', 'info'); ?>
+
+            <?php
+                global $wp_query;
+
+                $args = array(
+                    'post_type'         => 'model',
+                    'post_status'       => 'publish',
+                    'posts_per_page'    => -1,
+                    'orderby'           => 'date',
+                    'order'             => 'DESC',
+                );
+                $new_query = new WP_Query( $args );
+
+                if ($new_query->have_posts()) {
+                    echo '<ul class="products-list">';
+                    while ( $new_query->have_posts() ) : $new_query->the_post();
+
+                        get_template_part('inc/loop', 'model');
+
+                    endwhile;
+                    echo "</ul>";
+                } else {
+                    echo '<p class="no-results">' . __('Sorry, campers not found...', 'fw_campers') . '</p>';
+                }
+
+                wp_reset_query();
+            ?>
+
+        </div>
+    </section>
+
+<?php get_footer(); ?>

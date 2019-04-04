44ff7bdec018f06ef37e3e916de4176cdea2320e
Ihor <mazurenko.ihor@gmail.com>
2019-04-04 13:58:56 +0300

    wooocommerce add card icon to header

--- func/woocommerce-functions.php
+++ func/woocommerce-functions.php
@@ -68,3 +68,72 @@ function woocommerce_template_product_description() {
     wc_get_template( 'single-product/tabs/description.php' );
 }
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_product_description', 1000 );
+
+
+//add cart icon
+//Create Shortcode for WooCommerce Cart Menu Item
+add_shortcode ('woo_cart_but', 'woo_cart_but' );
+
+function woo_cart_but() {
+    ob_start();
+
+    $cart_count = WC()->cart->cart_contents_count; // Set variable for cart item count
+    $cart_url = wc_get_cart_url();  // Set Cart URL
+    $cart_class =  ( $cart_count > 0 ) ? '' : 'disable';
+
+    ?>
+    <li class="fwc-cart-box">
+        <a class="menu-item cart-contents <?php echo $cart_class; ?>" href="<?php echo $cart_url; ?>" title="<?php _e('My Basket','fw_campers') ?>">
+            <i class="fas fa-shopping-cart"></i>
+            <?php
+            if ( $cart_count > 0 ) {
+                ?>
+                <span class="cart-contents-count"><?php echo $cart_count; ?></span>
+                <?php
+            }
+            ?>
+        </a>
+    </li>
+    <?php
+
+    return ob_get_clean();
+
+}
+
+//Add AJAX Shortcode when cart contents update
+add_filter( 'woocommerce_add_to_cart_fragments', 'woo_cart_but_count' );
+
+function woo_cart_but_count( $fragments ) {
+
+    ob_start();
+
+    $cart_count = WC()->cart->cart_contents_count;
+    $cart_url = wc_get_cart_url();
+    $cart_class =  ( $cart_count > 0 ) ? '' : 'disable';
+
+    ?>
+    <a class="cart-contents menu-item <?php echo $cart_class; ?>" href="<?php echo $cart_url; ?>" title="<?php _e( 'View your shopping cart','fw_campers' ); ?>">
+        <i class="fas fa-shopping-cart"></i>
+        <?php
+            if ( $cart_count > 0 ) {
+                ?>
+                <span class="cart-contents-count"><?php echo $cart_count; ?></span>
+                <?php
+            }
+        ?>
+    </a>
+    <?php
+
+    $fragments['a.cart-contents'] = ob_get_clean();
+
+    return $fragments;
+}
+
+//Add WooCommerce Cart Menu Item Shortcode to particular menu
+add_filter( 'wp_nav_menu_main-menu_items', 'woo_cart_but_icon', 10, 2 ); // Change menu to suit - example uses 'top-menu'
+
+function woo_cart_but_icon ( $items, $args ) {
+    $items .=  do_shortcode('[woo_cart_but]'); // Adding the created Icon via the shortcode already created
+
+    return $items;
+}
